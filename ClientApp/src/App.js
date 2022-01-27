584955f324f0bd7dd8b9d37d4f597a736add13ea
Mel Incognito <melxincognito@gmail.com>
2022-01-27 14:19:15 -0700

    added redux with counter

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import { Counter } from "./components/Counter";
+import ClickMe from "./components/ClickMe";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-import './custom.css'
+import "./custom.css";
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+      <Provider store={store}>
+        <Layout>
+          <Route exact path="/" component={Home} />
+          <Route path="/counter" component={Counter} />
+          <Route path="/fetch-data" component={FetchData} />
+          <Route path="/estoyaqui" component={ClickMe} />
+        </Layout>
+      </Provider>
     );
   }
 }
---
+++ ClientApp/src/components/ClickMe.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../app/index";
+
+export default function ClickMe() {
+  const account = useSelector((state) => state.account); // if you just want the account you put state.account and it'll only display the account
+
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        alignContent: "center",
+        justifyContent: "center",
+        justifyItems: "center",
+        margin: "auto",
+      }}
+    >
+      <div className="logo">
+        <h1> Mel's Bank Account </h1>
+      </div>
+      <div className="accountInfo">
+        <h1> ${account}</h1>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <div className="depositWithdrawBtns">
+          <button onClick={() => depositMoney(100)}> Deposit $100 </button>
+          <button onClick={() => depositMoney(50)}> Deposit $50</button>
+          <button onClick={() => depositMoney(10)}> Deposit $10</button>
+          <button onClick={() => depositMoney(5)}> Deposit $5</button>
+          <button onClick={() => depositMoney(1)}> Deposit $1</button>
+        </div>
+        <div className="depositWithdrawBtns">
+          <button onClick={() => withdrawMoney(100)}> Withdraw $100 </button>
+          <button onClick={() => withdrawMoney(50)}> Withdraw $50</button>
+          <button onClick={() => withdrawMoney(10)}> Withdraw $10</button>
+          <button onClick={() => withdrawMoney(5)}> Withdraw $5</button>
+          <button onClick={() => withdrawMoney(1)}> Withdraw $1</button>
+        </div>
+      </div>
+    </div>
+  );
+}
